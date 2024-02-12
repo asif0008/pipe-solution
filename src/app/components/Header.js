5661e75576c28fd98b9ff4bb870d4a082255c85c
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2024-02-13 01:36:34 +0500

    css update

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -9,9 +9,9 @@ import Home from './Home'
 const Header = () => {
   return (
     <header className='bg-primary h-[4rem] my-auto'>
-        <div className='container-size flex items-center justify-between h-full text-white font-medium'>
+        <div className='container-size flex flex-col md:flex-row items-center justify-between h-full text-white font-medium'>
             <h4>Logo</h4>
-            <nav className='container-size flex items-center justify-center gap-[1.5rem]'>
+            <nav className='container-size flex items-center justify-center gap-2 md:gap-[1.5rem]'>
                 <Link href="/">Home</Link>  
                 <Link href="/about">About</Link>
                 <Link href="/contact">Contact</Link>
