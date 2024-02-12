e556e1972bf39026ab6aed85498177667a6e7870
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2024-02-13 04:23:01 +0500

    second and third section added

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -10,7 +10,7 @@ const About = () => {
     <section className='bg-cover bg-center bg-[url("/images/about-img-1.jpg")] h-screen'>
       <section className='container-size relative h-full'>
         <h1 
-        className='text-[2rem] sm:text-[3rem] md:text-[4rem] text-white font-semibold text-center pt-[9rem] md:pt-[9rem] pr-[2%]'
+        className='text-[2rem] sm:text-[3rem] md:text-[4rem] text-white font-semibold text-center pt-[10rem] md:pt-[9rem] pr-[2%]'
         >
           Pipe Solution Leader
         </h1>
@@ -19,14 +19,46 @@ const About = () => {
             Trusted by 200+ companies for 1000s of projects spanning the continent
           </p>
         </div>
-        <p className='text-white text-md pt-[4rem] md:pt-[15rem] w-full md:w-[30rem]'>
+        <p className='text-white text-md pt-[15rem] w-full md:w-[30rem]'>
         To integrate global expertise and excellence in PE and PB pipes and deliver comprehensive solutions, prioritizing unparalleled value for our clients and partners. 
         </p>
       </section>
-      {/* <Image src="" width={100} height={100} /> */}
     </section>
 
     {/* section two */}
+    <section className='bg-cover bg-center bg-[url("/images/about-img-2.jpg")] h-screen'>
+      <section className='container-size relative h-full'>
+          <p className='w-full md:w-[35rem] lg:w-[25rem] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-md text-center text-white'>
+          For 20+ years we have prioritised unparalleled value for our clients and partners.
+          </p>
+        <p className='text-white text-md absolute top-[70%] right-[0%] translate-x-[-0%] translate-y-[-70%] md:w-[17rem]'>
+        To deliver VIRTUE - in every aspect of our service. 
+        </p>
+      </section>
+    </section>
+
+    {/* section three */}
+    <section className='bg-cover bg-center bg-[url("/images/about-img-3.jpg")] h-screen'>
+      <section className='container-size relative h-full'>
+          <div className='absolute top-[40%] right-[0%] translate-x-[0%] translate-y-[-40%]'>
+            <h4 className='text-white text-[2rem] md:text-[3rem] font-semibold leading-10'>
+              <span className='text-[3rem] md:text-[5rem]'>V</span>alue
+            </h4>
+            <p className='text-white text-md md:w-[15rem]'>
+              Efficient operations provide  solutions without compromising affordability or quality.
+            </p>
+          </div>
+          <div className='absolute top-[70%] left-[0%] translate-x-[0%] translate-y-[-70%]'>
+            <h4 className='text-white text-[2rem] md:text-[3rem] font-semibold leading-none'>
+              <span className='text-[3rem] md:text-[5rem]'>I</span>nnovative <br />
+              Solutions
+            </h4>
+            <p className='text-white text-md md:w-[15rem]'>
+            Dedicated to evolving with industry trends, technologies, and client needs.
+            </p>
+          </div>
+      </section>
+    </section>
     <Footer />
     </>
     
