d695ddde3d5538c0350298bece43c6b5538d1f4f
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2024-02-13 01:39:20 +0500

    link added on logo

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -10,7 +10,9 @@ const Header = () => {
   return (
     <header className='bg-primary h-[4rem] my-auto'>
         <div className='container-size flex flex-col md:flex-row items-center justify-between h-full text-white font-medium'>
-            <h4>Logo</h4>
+           <Link href="/">
+               <h4>Logo</h4>
+           </Link>
             <nav className='container-size flex items-center justify-center gap-2 md:gap-[1.5rem]'>
                 <Link href="/">Home</Link>  
                 <Link href="/about">About</Link>
