5751c64597e4edb76a33a0c0069d97b9651868c3
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2024-02-16 00:05:08 +0500

    privacy page created

--- src/app/privacy-policy/page.js
+++ src/app/privacy-policy/page.js
@@ -1,15 +1,324 @@
 import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const Privacy = () => {
   return (
+    <>
+    <Header />
     <div className='bg-white'>
       <div className='container-size pt-[3rem] pb-[6rem]'>
       <h1 
           className='text-primary font-semibold text-[1.5rem] md:text-[2.5rem] border-b border-gray-300 pb-1'
           >Privacy Policy
       </h1>
+      <h2 className='mt-[3rem] text-xl font-medium'>
+      Introduction
+      </h2>
+      <p className='text-sm text-gray-600 leading-relaxed mt-2'>
+      Phetsiam PE Pipe Co., Ltd and its affiliates (as listed in the annex and hereinafter collectively referred to as "we") respect the rights to privacy of our customers (hereinafter referred to as "you"). To ensure that your personal data is protected, we have created this privacy notice to inform the details regarding the collection, use, disclosure, deletion, and destruction (collectively, the "processing ") of your personal data, both online and other channels, in accordance with the personal data protection law. 
+      </p>
+      <h2 className='mt-5 text-xl font-medium'>
+      1. Why We Process Your Personal Data
+      </h2>
+      <p className='text-sm text-gray-600 leading-relaxed mt-2'>
+      1.1 &nbsp; &nbsp; &nbsp; &nbsp; We process your personal data because it is necessary for the performance of the contracts between you and us. We use your personal data in this regard to, for example, provide you with services and products, manage your account, make deliveries, proceed with accounting and financial matters, give after-sales services, process returns or refunds. 
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      1.2 
+        <p>
+        We process yourpersonal data because it is necessary for the purposes of the legitimateinterests pursued by us or by a third party. Examples of what your personaldata is processed in this regard are as follows:
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (1)
+        <p>
+        To manage, develop, and conduct our business operations which include administration and development of websites and applications,fraud prevention and detection, crime prevention, relationship management forcurrent and potential customers, maintenance of information technology (IT)systems;
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (2)
+        <p>
+        To protect your security such as provision of security measures, protection of your personal data, access control and identity authentication when you log in to your user account; 
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (3)
+        <p>
+        To conduct marketing research and data analysis which includes sending news and privileges to you via emails, SMS, applications, social media, telephone, and direct mail, and to conduct questionnaires and interviews with you; and  
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (4)
+        <p>
+        To establish, exercise, or defend your or our legal claims.   
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      1.3 
+        <p>
+        We process your personal data because it is necessary in order to protect vital interests of you or of another person. We process your personal data to, for instance, make contact in case of emergency and control and prevent disease. 
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      1.4 
+        <p>
+        We process your personal data because it is necessary for compliance with a legal obligation to which we are subject. 
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      1.5
+        <p>
+        We process your personal data because it is necessary for the performance of a task carried out in the public interest or in the exercise of official authority vested in us.  
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      1.6
+        <p>
+        With your consent, we process your personal data to:  
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (1)
+        <p>
+        Enable us, the companies in our group, and our partners to send news and privileges to you via emails, SMS, applications, social media, telephone, and direct mail; and 
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (2)
+        <p>
+        Conduct other activities in which we might have to collect additional personal data about you in which case we will inform you and request your new consent from time to time. You can find out more about consent in item 4 of this privacy notice.  
+        </p>
+      </p>
+      <h2 className='mt-5 text-xl font-medium'>
+      2. Personal Data We Collect 
+      </h2>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      2.1
+        <p>
+        When you use our services, purchase our products, or become a member of our website, application; we collect the following personal data: 
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (1)
+        <p>
+        Personal information such as ID cards, ID number, title, name, date of birth, mobile number, email, marital status, conversation history, passport, Facebook account, LINE account, signature; 
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (2)
+        <p>
+        Contact information such as email address, telephone number, home address, office address, social media account; 
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (3)
+        <p>
+        Information related to your work such as occupation, position, work experience; 
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (4)
+        <p>
+        Information related to the purchase of products or services such as purchase history, claim history, complaints;  
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (5)
+        <p>
+        Personal data you have given to us when you contact us, request our after-sales services or participate in our research or interviews.   
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      2.2
+        <p>
+        When you visit our website or application or purchase our products or services online, we collect the following personal data: 
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (1)
+        <p>
+        Information about your registration such as name, email address, mobile number, passwords;   
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (2)
+        <p>
+        Information about electronic devices that you use, such as IP Address, location data, and another device identifier;    
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (3)
+        <p>
+        The type and version of the browser you use and the type and version of a plug-in;           
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (4)
+        <p>
+        Time Zone setting.                 
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      2.3
+        <p>
+        When you contact us or participate in any activity with us, such as contacting the contact centre, taking part in a customer satisfaction survey, and participating in our loyalty activities; we collect the following personal data:    
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (1)
+        <p>
+        Personal information, such as name, date of birth, photograph, identification card number, passport number, airline membership numbers (e.g. Royal Orchid Plus), health information (food allergies);                       
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (2)
+        <p>
+      	Contact information such as name, last name, telephone number, email, address;                            
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (3)
+        <p>
+      	Information about your participation in our activities, such as previous history of activities, and photographs taken during your participation.                           
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      2.4
+        <p>
+        We may need to collect and process special categories of personal data, as defined by personal data protection law, to fulfil the objectives we set out in this privacy notice such as     
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (1)
+        <p>
+        In some cases, we may collect your special categories of personal data even if the product or service you received is not directly associated with information such information. For instance, we do not intend to collect information about your religious belief but we obtain such information when we collect an image of your identification card; and                        
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (2)
+        <p>
+      	We might have to collect your health information, e.g. food allergies, when we organize a customer relationship activity for you.                           
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      2.5
+        <p>
+        Where necessary, we will process your special categories personal data only when you give us explicit consent or for other purposes as required by law. We will ensure that we will try our best to provide adequate security measures to protect your special categories personal data.      
+        </p>
+      </p>
+
+      <h2 className='mt-5 text-xl font-medium'>
+      3. Cookies and Other Similar Technologies  
+      </h2>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      3.1
+        <p>
+        Strictly Necessary Cookies. This type of cookie is strictly necessary to make the website function properly and safely, allow you to use the website, enable you to log in, verify your identity etc. 
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (1)
+        <p>
+        You cannot disable the use of this type of cookie via our website.
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      3.2
+        <p>
+        Analytic Cookies. These cookies collect information about your use of the website to enable us to assess, evaluate, improve, and develop our content, products/services and websites in order to enhance your experience of using our website. 
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (1)
+        <p>
+        If you do not allow us to use these cookies, we will not be able to measure, evaluate and develop websites.
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      3.3
+        <p>
+        Functional Cookies. This type of cookie helps to remember the computer or electronic data you use to visit the website, your registration or log in information, settings or options you have previously selected on the website, such as the language displayed on the website and shipping address. You will be able to use the website more conveniently without needing to provide information or reset every time you use the website.
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (1)
+        <p>
+        If you do not allow us to use these cookies, you may not be able to use the website easily and not efficiently.
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      3.4
+        <p>
+        Targeting Cookies. This type of cookie collects various information, which may include your personal data and create a profile about you so we can analyze and present products, services, and advertisements that best match your interests. 
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (1)
+        <p>
+        If you do not allow us to use these cookies, you may receive general information and advertisements that are not relevant to your interests.  
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      3.5
+        <p>
+        Third-party Cookies. Our website uses cookies belonging to third parties. The usage and settings will be in accordance with the first 4 types of cookies. You will not be able to select the settings for the specific cookies of third parties.
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2 ml-[3rem]'>
+      (1)
+        <p>
+        We are not able to control the use of that third party's data. You check the list of third parties and visit their websites to see their privacy notice and their cookies notice, which are different from ours.
+        </p>
+      </p>
+
+      <h2 className='mt-5 text-xl font-medium'>
+      4. Consent, Withdrawal and Consequences   
+      </h2>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      4.1
+        <p>
+        You are entitled to withdraw your consent at any time but such withdrawal will not affect the validity of the processing made prior to the withdrawal of consent. 
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      4.2
+        <p>
+        Your withdrawal of consent or refusal to provide certain information may result in us being unable to fulfil some or all of the objectives stated in this privacy notice.    
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      4.3
+        <p>
+        If you are under 20 years of age, you shall, prior to giving the consent, inform us of your parent or guardian so that we shall also be able to ask for consent from them.    
+        </p>
+      </p>
+
+      <h2 className='mt-5 text-xl font-medium'>
+      5. Retention Period    
+      </h2>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      5.1
+        <p>
+        We will retain your personal data for the period necessary to meet the objectives unless the law requires longer retention periods. In the event that such period is unclear, we will retain the data for a customary expected period in accordance with retention standards (e.g. the prescriptive period of 10 years for general legal claims).  
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      5.2
+        <p>
+        We have established an auditing system to delete or destroy your personal data when the retention period expires or when it becomes irrelevant or unnecessary for the purposes of collecting that personal data.       
+        </p>
+      </p>
+      <p className='flex items-start gap-[2.1rem] text-sm text-gray-600 leading-relaxed mt-2'>
+      5.3
+        <p>
+        If your personal data is processed based on consent, we will stop the processing when you have withdrawn the consent. However, we may keep your personal data to record your withdrawal so we can respond to your request in the future.      
+        </p>
+      </p>
       </div>
     </div>
+    <Footer />
+    </>
   )
 }
 
