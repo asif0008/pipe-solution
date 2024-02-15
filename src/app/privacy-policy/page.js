486a047b13e1d51f5b8933329f5fb33780f9b3da
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2024-02-15 22:57:59 +0500

    update in src folder

--- src/app/privacy-policy/page.js
+++ src/app/privacy-policy/page.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Privacy = () => {
   return (
-    <div></div>
+    <div className='bg-white'>
+      <div className='container-size pt-[3rem] pb-[6rem]'>
+      <h1 
+          className='text-primary font-semibold text-[1.5rem] md:text-[2.5rem] border-b border-gray-300 pb-1'
+          >Privacy Policy
+      </h1>
+      </div>
+    </div>
   )
 }
 
